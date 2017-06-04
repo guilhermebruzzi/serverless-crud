e0d58f1daecd8f462048ca99d242e118c37010a6
Guilherme Bruzzi <guibruzzi@gmail.com>
2017-06-04 14:02:55 -0300

    Fix update todos

--- todos-update.js
+++ todos-update.js
@@ -6,18 +6,26 @@ const dynamoDb = new AWS.DynamoDB.DocumentClient()
 module.exports = (event, callback) => {
   const data = JSON.parse(event.body)
 
-  data.id = event.pathParameters.id
-  data.updatedAt = new Date().getTime()
-
   const params = {
     TableName: 'todos',
-    Item: data,
+    Key: {
+      id: event.pathParameters.id,
+    },
+    UpdateExpression: 'set updatedAt = :updatedAt, #name=:name',
+    ExpressionAttributeNames: {
+      '#name': 'name',
+    },
+    ExpressionAttributeValues: {
+      ':name': data.name,
+      ':updatedAt': new Date().getTime(),
+    },
+    ReturnValues: 'UPDATED_NEW',
   }
 
-  return dynamoDb.put(params, (error) => {
+  return dynamoDb.update(params, (error, data) => {
     if (error) {
       callback(error)
     }
-    callback(error, params.Item)
+    callback(error, data)
   })
 }
