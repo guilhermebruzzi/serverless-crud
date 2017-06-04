529ac220d8c581eb077ce171bb6e63cdb9867ceb
Guilherme Bruzzi <guibruzzi@gmail.com>
2017-06-04 12:01:41 -0300

    Initial commit

---
+++ todos-create.js
@@ -0,0 +1,25 @@
+'use strict';
+
+const AWS = require('aws-sdk');
+const dynamoDb = new AWS.DynamoDB.DocumentClient();
+const uuid = require('uuid');
+
+module.exports = (event, callback) => {
+  const data = JSON.parse(event.body);
+
+  data.id = uuid.v1();
+  data.createdAt = new Date().getTime();
+  data.updatedAt = new Date().getTime();
+
+  const params = {
+    TableName: 'todos',
+    Item: data
+  };
+
+  return dynamoDb.put(params, (error, data) => {
+    if (error) {
+      callback(error);
+    }
+    callback(error, params.Item);
+  });
+};
